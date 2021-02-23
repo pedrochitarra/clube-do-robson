45dfefd3912ee8daf945a6b4e98035792c12f057
Pedro Chitarra <pedrochitarra@hotmail.com>
2021-02-23 14:54:38 -0300

    Route fixed

--- front-end/src/http-common.js
+++ front-end/src/http-common.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // baseURL: /api for build
 
 export default axios.create({
-  baseURL: 'https://clubedorobson.herokuapp.com/api',
+  baseURL: 'https://clubedorobson.herokuapp.com:5000/api',
   headers: {
     'Content-type': 'application/json'
   }
