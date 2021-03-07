b5b4157371d2641f1bc60b605157395df5ebd115
Pedro Chitarra <pedrochitarra@hotmail.com>
2021-03-07 13:48:33 -0300

    First Mobile Build

--- front-end/src/http-common.js
+++ front-end/src/http-common.js
@@ -4,9 +4,9 @@ import axios from 'axios'
 // baseURL: 'https://clubedorobson.herokuapp.com/api' for build
 
 export default axios.create({
-  // baseURL: 'https://clubedorobson.herokuapp.com/api',
+  baseURL: 'https://clubedorobson.herokuapp.com/api',
   // baseURL: 'http://localhost:5500/api',
-  baseURL: 'http://192.168.0.14:5500/api',
+  // baseURL: 'http://192.168.0.14:5500/api',
   headers: {
     'Content-type': 'application/json'
   }
